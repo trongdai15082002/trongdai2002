4dde89d43e92f2bf747e122c19f7ede3d13c4f86
trongdai15082002 <89824288+trongdai15082002@users.noreply.github.com>
2021-09-25 13:31:23 +0700

    Delete input.py

--- input.py
+++
@@ -1,6 +0,0 @@
-age = input("How old are you?")
-name = input("What your name?")
-
-message = "I am {}. I am {} years old."
-#print("I am " + name + ". I am 20" + age +" years old.")
-print(message.format(name, age))
